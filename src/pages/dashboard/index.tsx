37dc05a207d06d1a46b119b1f354746bfe7c4378
Kingd <divahsmart@gmail.com>
2024-08-02 16:44:25 +0200

    other components removed

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import Grid from '@mui/material/Grid';
 import TopCards from 'components/sections/dashboard/top-cards';
-import WebsiteVisitors from 'components/sections/dashboard/website-visitors';
-import RevenueByCustomer from 'components/sections/dashboard/revenue-by-customer';
+// import WebsiteVisitors from 'components/sections/dashboard/website-visitors';
+// import RevenueByCustomer from 'components/sections/dashboard/revenue-by-customer';
 import Products from 'components/sections/dashboard/products';
 import CompletedTask from 'components/sections/dashboard/completed-task';
 import OrdersStatus from 'components/sections/dashboard/orders-status';
@@ -13,13 +13,13 @@ const Dashboard = () => {
         <TopCards />
       </Grid>
 
-      <Grid item xs={12} xl={4}>
+      {/* <Grid item xs={12} xl={4}>
         <WebsiteVisitors />
       </Grid>
 
       <Grid item xs={12} xl={8}>
         <RevenueByCustomer />
-      </Grid>
+      </Grid> */}
 
       <Grid item xs={12} xl={4}>
         <Products />
